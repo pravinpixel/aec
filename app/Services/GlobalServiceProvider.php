a632e171c11757be03311fa56e24211684f3ee5b
prabhu-pixel <prabhu.k.pixel@gmail.com>
2021-12-23 10:42:29 +0530

    added global service

---
+++ app/Services/GlobalServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+
+use App\Http\Controllers\Controller;
+use App\Models\Config;
+// use PhpParser\Node\Stmt\Switch_;
+
+class GlobalServiceProvider extends Controller
+{
+   
+    public function getMenus()
+    {
+        
+    }
+
+    public function enquiryNumber()
+    {
+        $config = $this->getConfig();
+        return "{$config->enquiry_prefix}-{$config->enquiry_number}";
+    }
+
+    public function getConfig()
+    {
+        return Config::first();
+    }
+
+    public function updateConfig($type)
+    {
+        $config = $this->getConfig();
+        switch ($type) {
+            case 'ENQ':
+                $config->enquiry_number += 1;
+                $config->save();
+                break;
+            default:
+                
+                break;
+        }
+    }
+}
