0e8d270ff6fec94d06b57d059c00921cb7e2adbd
vetri-pixel <vetriselvam.pixel@gmail.com>
2022-02-11 13:04:51 +0530

    added upload delete option

--- resources/views/customer/enquiry/view-list.blade.php
+++ resources/views/customer/enquiry/view-list.blade.php
@@ -20,10 +20,16 @@
             <td class="text-center"  ng-show="viewList.pivot.file_type != 'link'">
                 <a download="{{ asset("public/uploads/") }}/@{{ viewList.pivot.file_name }}" href="{{ asset("public/uploads/") }}/@{{ viewList.pivot.file_name }}"><i class="fa fa-download btn-sm rounded-pill btn btn-outline-primary"></i></a>
                 <a target="_child" href="{{ asset("public/uploads/") }}/@{{ viewList.pivot.file_name }}"><i class="fa fa-eye btn-sm rounded-pill btn btn-outline-info"></i></a>
+                <a href="" custom-modal="modal" modal-title="Delete" modal-body="Are you sure to perform this action" modal-route="{{ route('customers.enquiry-document') }}" modal-enquiry-id="@{{  viewList.pivot.enquiry_id }}"   modal-view-type="@{{  viewList.slug }}"  modal-id="@{{  viewList.pivot.id }}"  modal-method="DELETE" >  
+                    <i class="feather-trash btn-outline-danger btn btn-sm rounded-pill mr-3"> </i>
+                </a> 
             </td> 
             
             <td class="text-center"  ng-show="viewList.pivot.file_type == 'link'">
                 <a class="" target="_blank" href="@{{ viewList.pivot.file_name }}"><i class="fa fa-eye btn-sm rounded-pill btn btn-outline-info"></i></a>
+                <a href="" custom-modal="modal" modal-title="Delete" modal-body="Are you sure to perform this action" modal-route="{{ route('customers.enquiry-document') }}" modal-enquiry-id="@{{  viewList.pivot.enquiry_id }}"   modal-view-type="@{{  viewList.slug }}"  modal-id="@{{  viewList.pivot.id }}"  modal-method="DELETE" >  
+                    <i class="feather-trash btn-outline-danger btn btn-sm rounded-pill mr-3"> </i>
+                </a> 
             </td> 
         </tr> 
     </tbody>
