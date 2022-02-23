e1a404d665f82c4f6ca8de4b9a7078ba9822ff87
vetri-pixel <vetriselvam.pixel@gmail.com>
2022-02-23 18:09:07 +0530

    AP6-I52 ui changes

--- resources/views/customer/enquiry/table/view-list.blade.php
+++ resources/views/customer/enquiry/table/view-list.blade.php
@@ -13,8 +13,8 @@
     <tbody class="panel"> 
         <tr ng-repeat="viewList in viewLists">
             <td> @{{ $index + 1}}  @{{ viewList.name }}</td>
-                <td>@{{  viewList.pivot.date }}</td>
-                <td>@{{  viewList.pivot.client_file_name }}</td>
+                <td >@{{  viewList.pivot.date }}</td>
+                <td style="max-width: 200px !important;overflow:hidden">@{{  viewList.pivot.client_file_name }}</td>
                 <td class="text-success">@{{  viewList.pivot.file_type }}</td>
             <td><span class="badge btn-sm badge-outline-success rounded-pill"> @{{  viewList.pivot.status }} </span></td>
             <td class="ext-start"  ng-show="viewList.pivot.file_type != 'link'">
