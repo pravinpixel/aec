1bf82793d7fba2dc0c00397a5390076e9f27d68f
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-03-10 11:20:54 +0530

    server key

--- app/Repositories/PushNotificationRepository.php
+++ app/Repositories/PushNotificationRepository.php
@@ -7,8 +7,9 @@ class PushNotificationRepository implements PushNotificationRepositoryInterface
 		// return [$message_type, $chat_type];
         // Customer()
         
-        $SERVER_API_KEY = 'AAAA0L4qpng:APA91bG9QBnGO2Sx5iws0i32Qo5SLPiN3HwYU2vMYWJBinF5JGfm2s_434VuaMno-KRw2ZSSi12XdzMmcLlcXpQL51Ea1B2lkQymMSAWaELSU-laofEhcT6k64jUq0mi2b3rvgapPk7k';
+        $SERVER_API_KEY = 'AAAA0L4qpng:APA91bEbhnagIiPglRpPHwR0Hg1niG9kv6eFBqpPpqXUERAQy5NcmIs47AfwPyOcT5F3azQWh8Zm3DAz6dkkZ2aDiSl3Ln-5P6fny254WpmJfbsZGxrPjvKzju8pkkcRKOty_8ec7SMY';
         // $firebaseToken  = 'elCrAarDzYVJvVLYplXMGq:APA91bHZyaGiG_mP65R4IT-VTGPG88-ou2a0-iaCKQhrBEgqmUAfOK6if5nKAyIYeyspgx39NXIuQjZx5Lu9BYaYB2SmUaU_xPf2rTKN-OJNONdT_HekJGu_QyymE6kBR2D2FMV09Fx8';
+        
         $data = [
             "registration_ids" => $firebaseToken,
             "notification" => [
