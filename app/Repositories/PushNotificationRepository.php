da2895923fbb074f3c3f24855a223846a350181a
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-03-10 11:36:00 +0530

    firebaseToken correction 02

--- app/Repositories/PushNotificationRepository.php
+++ app/Repositories/PushNotificationRepository.php
@@ -12,7 +12,7 @@ class PushNotificationRepository implements PushNotificationRepositoryInterface
         $firebaseToken_old  = 'elCrAarDzYVJvVLYplXMGq:APA91bHZyaGiG_mP65R4IT-VTGPG88-ou2a0-iaCKQhrBEgqmUAfOK6if5nKAyIYeyspgx39NXIuQjZx5Lu9BYaYB2SmUaU_xPf2rTKN-OJNONdT_HekJGu_QyymE6kBR2D2FMV09Fx8';
          
         $data = [
-            "registration_ids" => $firebaseToken[0],
+            "registration_ids" => [$firebaseToken[0]],
             "notification" => [
                 "title" => "Testing Title",
                 "body" => "Testing Body",
