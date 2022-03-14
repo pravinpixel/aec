b6bdaf5f4d4632806e7e9d138b476dfadd9a8ba3
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-03-14 13:17:19 +0530

    ajax remove

--- resources/views/admin/dashboard/enquiry.blade.php
+++ resources/views/admin/dashboard/enquiry.blade.php
@@ -449,8 +449,7 @@
 @endpush
 
 @if ($adminData->notification == 1)
-	<script src="https://www.gstatic.com/firebasejs/7.23.0/firebase.js"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+	<script src="https://www.gstatic.com/firebasejs/7.23.0/firebase.js"></script> 
 	<script>
 
 		var firebaseConfig = {
