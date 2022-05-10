9162d6d7d6d11c989902b867016faebb75f77fea
vetri-pixel <vetriselvam.pixel@gmail.com>
2022-05-10 15:37:49 +0530

    partition wall changes

--- database/seeders/BuildingComponentSeeder.php
+++ database/seeders/BuildingComponentSeeder.php
@@ -16,7 +16,7 @@ class BuildingComponentSeeder extends Seeder
     {
         $externalWall = BuildingComponent::create(['building_component_name' => 'External Wall','building_component_icon' => 'dripicons-store', 'top_position'=> 'Exterior',  'bottom_position'=> 'Interior',  'order_id'=> 1,'is_active' => 1]);
         $internalWall = BuildingComponent::create(['building_component_name' => 'Internal Wall', 'building_component_icon' =>'uil uil-store-alt', 'top_position'=> '',  'bottom_position'=> '', 'order_id'=> 2,'is_active' => 1]);
-        $partitionWall = BuildingComponent::create(['building_component_name' => 'Partition Wall', 'building_component_icon' =>'uil uil-wall', 'top_position'=> '',  'bottom_position'=> '', 'order_id'=> 3, 'is_active' => 1]);
+        $partitionWall = BuildingComponent::create(['building_component_name' => 'Fire and Sound', 'building_component_icon' =>'uil uil-wall', 'top_position'=> '',  'bottom_position'=> '', 'order_id'=> 3, 'is_active' => 1]);
         $ceiling = BuildingComponent::create(['building_component_name' => 'Ceiling', 'building_component_icon' =>'uil uil-layers', 'top_position'=> '',  'bottom_position'=> '', 'order_id'=> 4,'is_active' => 1]);
         $roof = BuildingComponent::create(['building_component_name' => 'Roof',  'building_component_icon' =>'uil uil-mountains-sun', 'top_position'=> 'Exterior',  'bottom_position'=> 'Bottom', 'order_id'=> 5, 'is_active' => 1]);
 
