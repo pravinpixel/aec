60a4e60859931a21145716d1f795101f72b59296
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-06-27 19:45:10 +0530

    signup

--- app/Http/Controllers/Auth/UserController.php
+++ app/Http/Controllers/Auth/UserController.php
@@ -17,4 +17,4 @@ class UserController extends Controller
         
     }
     
-}
+} 
--- resources/views/auth/customer/signup.blade.php
+++ resources/views/auth/customer/signup.blade.php
@@ -7,24 +7,29 @@
             <div class="row justify-content-center">
                 <div class="col-xxl-4 col-lg-5 pt-md-5">
                     <div class="card ">  
-                        <div class="card-body pt-0 p-4">                            
-                            <div class="text-center pt-4 mb-3 w-75 m-auto">
+                        <div class="card-header border-primary">
+                            <div class="text-center py-2 w-75 m-auto">
                                 <span><img src="{{ asset("public/assets/images/logo_customer.png") }}" alt="{{ env('APP_NAME') }}" width="150px"></span>
                             </div>
-
+                        </div>
+                        <div class="card-body p-4"> 
                             <form class="form-horizontal" method="post" action="{{ route('login') }}">
                                 @csrf
-                                <h4 class="text-dark-50 bg-light p-2 text-center text-primary  mb-3"> <i class="fa fa-user"></i> Customer Registration</h4>
-
-
-                                <div class="mb-3">
-                                    <label for="emailaddress" class="form-label">Email address</label>
-                                    <input class="form-control" type="email" name="email" id="emailaddress" required="" placeholder="Enter your email">
+                                <h4> Create Account</h4>
+                                <div class="my-3">
+                                    <label for="first_name" class="form-label text-secondary">First Name</label>
+                                    <input class="form-control" type="text" name="first_name" id="first_name" required="" placeholder="Enter your email">
                                 </div>
-
-                                <div class="mb-3">
-                                    
-                                    <label for="password" class="form-label">Password</label>
+                                <div class="my-3">
+                                    <label for="last_name" class="form-label text-secondary">Last Name</label>
+                                    <input class="form-control" type="text" name="last_name" id="last_name" required="" placeholder="Enter your email">
+                                </div>
+                                <div class="my-3">
+                                    <label for="emailaddress" class="form-label text-secondary">Email address</label>
+                                    <input class="form-control" type="email" name="email" id="emailaddress" required="" placeholder="Enter your email">
+                                </div> 
+                                <div class="mb-3"> 
+                                    <label for="password" class="form-label text-secondary">Password</label>
                                     <div class="input-group input-group-merge">
                                         <input type="password" id="password" name="password" class="form-control" placeholder="Enter your password">
                                         <div class="input-group-text" data-password="false">
@@ -32,14 +37,14 @@
                                         </div>
                                     </div>
                                     {{-- <a href="#" class="text-muted float-end"><small>Forgot your password?</small></a> --}}
-                                </div>
-
-                              
+                                </div> 
 
                                 <div class="  mb-0 text-center">
-                                    <button class="btn btn-primary  w-100" type="submit"> Log In </button>
+                                    <button class="btn btn-info fw-bold w-100" type="submit"> Sign Up </button>
                                 </div>
-
+                                <div class="text-center mt-3">
+                                    Already have a account ? <a href="" class="text-info">Forget Password ?</a>
+                                </div> 
                             </form>
                         </div> <!-- end card-body -->
                     </div>
