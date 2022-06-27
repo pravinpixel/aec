8bd23bb11b6f0800d0ef166a2a9fa6a2d5d5c847
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-06-27 17:02:10 +0530

    Waiting for verification ui changes

--- resources/views/admin/enquiry/wizard/wood-estimation.blade.php
+++ resources/views/admin/enquiry/wizard/wood-estimation.blade.php
@@ -2,10 +2,13 @@
 @if(userHasAccess('cost_estimate_add')) 
     <div class="row mx-0 align-items-center" >
         <div class="col-md-4 p-0">
-            <span ng-show="cost_estimate.assign_for == 'verification'" class="alert d-flex align-items-center m-0 shadow border-success border alert-success bg-transparent text-success" role="alert">
+            <span ng-show="cost_estimate.assign_for == 'verification' && cost_estimate.assign_for_status == 1" class="alert d-flex align-items-center m-0 shadow border-success border alert-success bg-transparent text-success" role="alert">
                 <strong class="fa fa-check-circle fa-2x me-2" aria-hidden="true"></strong>
-                <span ng-show="cost_estimate.assign_for == 'verification' && cost_estimate.assign_for_status == 0"> Waiting for verification</span>
                 <span ng-show="cost_estimate.assign_for == 'verification' && cost_estimate.assign_for_status == 1"> Verification completed successfully</span>
+            </span>
+            <span  ng-show="cost_estimate.assign_for == 'verification' && cost_estimate.assign_for_status == 0" class="alert d-flex align-items-center m-0 shadow border-warning border alert-warning bg-transparent text-warning" role="alert">
+                <strong class="fa fa-exclamation-circle fa-2x me-2" aria-hidden="true"></strong>
+                <span> Waiting for verification</span>
             </span>
         </div>
         <div class="col-md-8 p-0">
