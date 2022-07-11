b5d12937372c20c37c259790e9346dd54d9e8e1f
vetri-pixel <vetriselvam.pixel@gmail.com>
2022-07-11 18:46:45 +0530

    document seeder updated

--- database/seeders/DocumentTypeSeeder.php
+++ database/seeders/DocumentTypeSeeder.php
@@ -16,6 +16,6 @@ class DocumentTypeSeeder extends Seeder
     {
         DocumentType::create(['document_type_name' => 'Drawings' , 'slug' => 'Drawings', 'is_active' => 1]);
         DocumentType::create(['document_type_name' => '3D Model' ,'slug' => 'Three_dimensional_model', 'is_active' => 1]);
-        DocumentType::create(['document_type_name' => 'Foundation Drawings & Other' ,'slug' => 'Foundation_drawings_other', 'is_active' => 1]);
+        DocumentType::create(['document_type_name' => 'Foundation Drawings & Other' ,'slug' => 'Foundation_drawings_and_other', 'is_active' => 1]);
     }
 }
