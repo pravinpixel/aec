8d148dc4e685f8a27c1f5d27fe71d3ea9e5b3dad
vetri-pixel <vetriselvam.pixel@gmail.com>
2022-07-15 11:57:11 +0530

    comments editable

--- resources/views/customer/enquiry/wizard/review.blade.php
+++ resources/views/customer/enquiry/wizard/review.blade.php
@@ -382,7 +382,7 @@
                 <div id="add_info" class="accordion-collapse collapse  " aria-labelledby="add_info_header" >
                     <div class="accordion-body">  
                         <div class="form-floating" id="additional_info_text_editor" style="pointer-events: none">
-                            <div dx-html-editor="htmlEditorOptions" contenteditable="false"> </div>
+                            <div dx-html-editor="htmlEditorOptions" contenteditable="true"> </div>
                             </div>
                         </div>
                     </div> 
