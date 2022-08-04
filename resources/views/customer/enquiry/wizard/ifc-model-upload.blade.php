37f340d79639d21120e8bef8aad7d3af2315ea88
vetri-pixel <vetriselvam.pixel@gmail.com>
2022-08-04 18:01:41 +0530

    file upload validation

--- resources/views/customer/enquiry/wizard/ifc-model-upload.blade.php
+++ resources/views/customer/enquiry/wizard/ifc-model-upload.blade.php
@@ -1,4 +1,4 @@
-<form ng-submit="submitIFC()">
+<form ng-submit="submitIFC()" name="ifcForm" ng-model="ifcForm">
 
         <div class="row">
             <div ng-repeat="documentType in documentTypes" class="col-md-4">
@@ -9,8 +9,8 @@
                     <input  type="file" file-drop-zone="@{{'file' + documentType.slug}}" class="form-control file-control rounded-pill" file-model="@{{'file' + documentType.slug}}" id ="@{{ documentType.slug }}"/>
                     </div>
                     <small class="text-center my-1">(OR)</small>
-                    <input type="text" id="@{{'link' +documentType.slug}}" class="form-control rounded-pill border" placeholder="URL">
-                    <a ng-click="uploadFile(documentType.slug, documentType.slug)"  class="fileupload btn btn-primary rounded-pill border-primary mt-2"><i class="fa fa-upload"></i> Upload</a>
+                    <input type="url" id="@{{'link' +documentType.slug}}" class="form-control rounded-pill border" name="url" ng-model="url" placeholder="URL">
+                    <a ng-disabled="ifcForm.!$invalid" ng-click="uploadFile(documentType.slug, documentType.slug)"  class="fileupload btn btn-primary rounded-pill border-primary mt-2"><i class="fa fa-upload"></i> Upload</a>
                     <div class="progress mt-2">
                         <div ng-show="@{{documentType.slug+'showProgress'}}" class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width:@{{progress_value}}"> @{{progress_value}} </div>
                     </div>
