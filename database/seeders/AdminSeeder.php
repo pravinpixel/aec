98172b27e6ef3b00961a3c2bd225493ceea9a890
vetri-pixel <vetriselvam.pixel@gmail.com>
2022-08-04 13:03:12 +0530

    added admin seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -31,5 +31,51 @@ class AdminSeeder extends Seeder
             'job_role' => 1,
             'reference_number' => 'EMP-1',
         ]);
+        $admin = Employees::create([
+            'first_name' => 'aecadmnusr',
+            'last_name' => '2',
+            'user_name' => 'Alan',
+            'email' => 'aecadmnusr1@gmail.com',
+            'password' => Hash::make('12345678'),
+            'mobile_number' => '98786757',
+            // 'share_access' => 1, 
+            'bim_access' => 1, 
+            // 'access' => 1, 
+            'status' => 1,
+            'image' => '',
+            'job_role' => 1,
+            'reference_number' => 'EMP-2',
+        ]);
+        $admin = Employees::create([
+            'first_name' => 'aecadmnusr',
+            'last_name' => '2',
+            'user_name' => 'Rosan',
+            'email' => 'aecadmnusr2@gmail.com',
+            'password' => Hash::make('12345678'),
+            'mobile_number' => '98786758',
+            // 'share_access' => 1, 
+            'bim_access' => 1, 
+            // 'access' => 1, 
+            'status' => 1,
+            'image' => '',
+            'job_role' => 1,
+            'reference_number' => 'EMP-3',
+        ]);
+
+        $admin = Employees::create([
+            'first_name' => 'denial',
+            'last_name' => 'dev',
+            'user_name' => 'denial',
+            'email' => 'denial@aecprefab.net',
+            'password' => Hash::make('12345678'),
+            'mobile_number' => '98786759',
+            // 'share_access' => 1, 
+            'bim_access' => 1, 
+            // 'access' => 1, 
+            'status' => 1,
+            'image' => '',
+            'job_role' => 5,
+            'reference_number' => 'EMP-4',
+        ]);
     }
 }
