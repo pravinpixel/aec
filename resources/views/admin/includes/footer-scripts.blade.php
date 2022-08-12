5c45a0394fe7137a5dddeea3949a699dc3bd08ce
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-08-12 16:28:27 +0530

    Live Project UI Changes

--- resources/views/admin/includes/footer-scripts.blade.php
+++ resources/views/admin/includes/footer-scripts.blade.php
@@ -44,7 +44,7 @@
 
 <script src="{{ asset('public/assets/js/vendor/jquery.dataTables.min.js') }}"></script>
 <script src="{{ asset('public/assets/js/vendor/dataTables.bootstrap5.js') }}"></script>
-<script src="{{ asset('public/assets/js/vendor/dataTables.responsive.min.js') }}"></script>
+{{-- <script src="{{ asset('public/assets/js/vendor/dataTables.responsive.min.js') }}"></script> --}}
 <script src="{{ asset('public/assets/js/vendor/responsive.bootstrap5.min.js') }}"></script>
 <script src="{{ asset('public/assets/js/vendor/dataTables.buttons.min.js') }}"></script>
 <script src="{{ asset('public/assets/js/vendor/buttons.bootstrap5.min.js') }}"></script>
