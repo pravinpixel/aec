00725c6488560933e55aaa613478f6ce2b032803
vetri-pixel <vetriselvam.pixel@gmail.com>
2022-08-18 11:51:40 +0530

    migration changes

--- database/migrations/2022_07_01_131640_create_ticket_comments.php
+++ database/migrations/2022_07_01_131640_create_ticket_comments.php
@@ -16,11 +16,14 @@ class CreateTicketComments extends Migration
         Schema::create('ticket_comments', function (Blueprint $table) {
             $table->id();
             $table->unsignedInteger('project_id');
+            $table->integer('ticket_num')->nullable();
             $table->string("type")->nullable();
             $table->text('summary')->nullable();
             $table->text('description')->nullable();
             $table->text('file_id')->nullable();
             $table->text('assigned')->nullable();
+            $table->string("cus_tag")->nullable();
+            $table->string("emp_tag")->nullable();
             $table->text('priority')->nullable();
             $table->datetime('ticket_date')->nullable();
             $table->boolean('status')->default(0);
@@ -33,6 +36,7 @@ class CreateTicketComments extends Migration
             $table->unsignedBigInteger('send_by')->nullable();
             $table->unsignedBigInteger('seen_by')->nullable();
             $table->string('role_by')->nullable();
+            $table->boolean('is_active')->default(1);
             
             $table->timestamps();
         });
