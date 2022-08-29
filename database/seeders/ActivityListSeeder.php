e12190398fec2bf61730341e930483062ced1c8b
pixel-santhosh <santhoshd.pixel@gmail.com>
2022-08-29 15:45:21 +0530

    activity seed

--- database/seeders/ActivityListSeeder.php
+++ database/seeders/ActivityListSeeder.php
@@ -19,5 +19,20 @@ class ActivityListSeeder extends Seeder
         ActivityList::create(['name' => 'Structural Element Drawing', 'is_active' => 1]);
         ActivityList::create(['name' => 'Foundation Concrete Drawing(optional)', 'is_active' => 1]);
         ActivityList::create(['name' => 'Window and door legend drawings', 'is_active' => 1]);
+
+        ActivityList::create(['name' => 'External and Internal wall Fabrication drawings', 'is_active' => 1]);
+        ActivityList::create(['name' => 'Floor Fabrication drawings', 'is_active' => 1]);
+        ActivityList::create(['name' => 'Roof Fabrication drawings', 'is_active' => 1]);
+        ActivityList::create(['name' => 'Beam and column manufacturing drawings', 'is_active' => 1]);
+
+        ActivityList::create(['name' => 'External and Internal wall Fabrication drawings', 'is_active' => 1]);
+        ActivityList::create(['name' => 'Floor Fabrication drawings', 'is_active' => 1]);
+        ActivityList::create(['name' => 'Roof Fabrication drawings', 'is_active' => 1]);
+        ActivityList::create(['name' => '3D Installation drawings ( optional)', 'is_active' => 1]);
+        ActivityList::create(['name' => 'Total Material List(Timber and steel components)', 'is_active' => 1]);
+        ActivityList::create(['name' => 'Foundation sill Drawings)', 'is_active' => 1]);
+        ActivityList::create(['name' => 'Transport Packaging Drawing(client Optional)', 'is_active' => 1]);
+        ActivityList::create(['name' => 'CNC DATA (client Optional)', 'is_active' => 1]);
+        ActivityList::create(['name' => 'Find set of drawings uploaded in BIM 360', 'is_active' => 1]);
     }
 }
