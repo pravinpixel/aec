5a0310b1704d0c6f79e841432d717a2277f9f91b
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-08-30 13:22:09 +0530

    Admin Top  Nav Ui Changes

--- resources/views/admin/includes/top-bar.blade.php
+++ resources/views/admin/includes/top-bar.blade.php
@@ -1,258 +1,108 @@
- 
-    <!-- Topbar Start -->
-    <div class="navbar-custom">
-        <ul class="list-unstyled topbar-menu float-end mb-0">
-            <li class="notification-list">
-                <a class="nav-link end-bar-toggle"  title="background process" href="{{ url('/jobs') }}">
-                    <i class="dripicons-calendar noti-icon"></i>
-                </a>
-            </li>
-            {{-- <li class="dropdown notification-list d-lg-none">
-                <a class="nav-link dropdown-toggle arrow-none" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                    <i class="dripicons-search noti-icon"></i>
-                </a>
-                <div class="dropdown-menu dropdown-menu-animated dropdown-lg p-0">
-                    <form class="p-3">
-                        <input type="text" class="form-control" placeholder="Search ..." aria-label="Recipient's username">
-                    </form>
-                </div>
-            </li>
-            <li class="dropdown notification-list topbar-dropdown">
-                <a class="nav-link dropdown-toggle arrow-none" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                    <img src="{{ asset('public/assets/images/flags/us.jpg') }}" alt="user-image" class="me-0 me-sm-1" height="12"> 
-                    <span class="align-middle d-none d-sm-inline-block">English</span> <i class="mdi mdi-chevron-down d-none d-sm-inline-block align-middle"></i>
-                </a>
-                <div class="dropdown-menu dropdown-menu-end dropdown-menu-animated topbar-dropdown-menu">
-
-                    <!-- item-->
-                    <a href="javascript:void(0);" class="dropdown-item notify-item">
-                        <img src="{{ asset('public/assets/images/flags/germany.jpg') }}" alt="user-image" class="me-1" height="12"> <span class="align-middle">German</span>
-                    </a>
-
-                    <!-- item-->
-                    <a href="javascript:void(0);" class="dropdown-item notify-item">
-                        <img src="{{ asset('public/assets/images/flags/italy.jpg') }}" alt="user-image" class="me-1" height="12"> <span class="align-middle">Italian</span>
-                    </a>
-
-                    <!-- item-->
-                    <a href="javascript:void(0);" class="dropdown-item notify-item">
-                        <img src="{{ asset('public/assets/images/flags/spain.jpg') }}" alt="user-image" class="me-1" height="12"> <span class="align-middle">Spanish</span>
-                    </a>
-
-                    <!-- item-->
-                    <a href="javascript:void(0);" class="dropdown-item notify-item">
-                        <img src="{{ asset('public/assets/images/flags/russia.jpg') }}" alt="user-image" class="me-1" height="12"> <span class="align-middle">Russian</span>
-                    </a>
-
-                </div>
-            </li> --}}
-
-            <li class="dropdown notification-list">
-                
-                <a class="nav-link dropdown-toggle arrow-none" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                    <i class="dripicons-bell noti-icon"></i>
-                    <span class="noti-icon-badge"></span>
-                </a>
-                <div class="dropdown-menu dropdown-menu-end dropdown-menu-animated dropdown-lg">
-
-                    <!-- item-->
-                    <div class="dropdown-item noti-title">
-                        <h5 class="m-0">
-                            <span class="float-end">
-                                <a href="javascript: void(0);" class="text-dark">
-                                    <small>Clear All</small>
-                                </a>
-                            </span>Notification
-                        </h5>
-                    </div>
-
-                    <div style="max-height: 230px;" data-simplebar>
-                        {{-- <!-- item-->
-                        <a href="javascript:void(0);" class="dropdown-item notify-item">
-                            <div class="notify-icon bg-primary">
-                                <i class="mdi mdi-comment-account-outline"></i>
-                            </div>
-                            <p class="notify-details">Caleb Flakelar commented on Admin
-                                <small class="text-muted">1 min ago</small>
-                            </p>
-                        </a> --}}
-
-                        <!-- item-->
-                        <a href="javascript:void(0);" class="dropdown-item notify-item">
-                            <div class="text-center">There is no new notification.!</div>
-                        </a>
-
-                         
-                    </div>
-
-                    <!-- All-->
-                    <a href="javascript:void(0);" class="dropdown-item text-center text-primary notify-item notify-all">
-                        View All
-                    </a>
-
-                </div>
-            </li>
-
-            @if(userHasAccess('gantt_chart_index') || userHasAccess('cost_estimate_calculation_index'))
-                <li class="dropdown notification-list d-none d-sm-inline-block">
-                    <a class="nav-link dropdown-toggle arrow-none" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                        <i class="dripicons-view-apps noti-icon"></i>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-end dropdown-menu-animated dropdown-lg p-0">
-
-                        <div class="p-2 shadow-sm border">
-                            
-                            <div class="row g-0 p-0 m-0">
-                                @if(userHasAccess('gantt_chart_index'))
-                                    <div class="col p-2">
-                                        <a class="dropdown-icon-item" href="{{ route('admin-gantt-chart-single-view') }}">
-                                            <img src="{{ asset('public/assets/images/gantt-chart.png') }}" alt="slack">
-                                            <span>Gantt chart</span>
-                                        </a>
-                                    </div>
-                                @endif
-                                @if(userHasAccess('cost_estimate_calculation_index'))
-                                    <div class="col p-2">
-                                        <a class="dropdown-icon-item" href="{{ route('enquiry.calculate-cost-estimation') }}">
-                                            <img src="{{ asset('public/assets/images/calculator.png') }}" alt="slack">
-                                            <span>Calculator</span>
-                                        </a>
-                                    </div>
-                                @endif
-                                {{-- <div class="col p-2">
-                                    <a class="dropdown-icon-item" href="#">
-                                        <img src="{{ asset('public/assets/images/brands/g-suite.png') }}" alt="G Suite">
-                                        <span>E search</span>
-                                    </a>
-                                </div> --}}
-                            </div> <!-- end row-->
-                        </div>
-                        
-                    </div>
-                </li>
-            @endif
-
-            <li class="notification-list">
-                <a class="nav-link end-bar-toggle" href="javascript: void(0);">
-                    <i class="dripicons-gear noti-icon"></i>
-                </a>
-            </li>
-
-            <li class="dropdown notification-list">
-                <a class="nav-link dropdown-toggle nav-user arrow-none me-0" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false"
-                    aria-expanded="false">
-                    <span class="account-user-avatar"> 
-                        <img src="{{ asset('public/assets/images/users/avatar-1.jpg') }}" alt="user-image" class="rounded-circle">
-                    </span>
-                    <span>
-                        <span class="account-user-name text-capitalize">{{ Admin()->full_name ?? 'Avatar' }}</span>
-                        {{-- <span class="account-position  text-capitalize">{{ userRole()->name ?? '' }}</span> --}}
-                    </span>
-                </a>
-                <div class="dropdown-menu dropdown-menu-end dropdown-menu-animated topbar-dropdown-menu profile-dropdown">
-            
-
-                    <!-- item-->
-                    <a href="javascript:void(0);" class="dropdown-item notify-item">
-                        <i class="mdi mdi-account-circle me-1"></i>
-                        <span>My Account</span>
-                    </a>
-
-                    {{-- <!-- item-->
-                    <a href="javascript:void(0);" class="dropdown-item notify-item">
-                        <i class="mdi mdi-account-edit me-1"></i>
-                        <span>Settings</span>
-                    </a> --}}
-
-                    {{-- <!-- item-->
-                    <a href="javascript:void(0);" class="dropdown-item notify-item">
-                        <i class="mdi mdi-lifebuoy me-1"></i>
-                        <span>Support</span>
-                    </a> --}}
-
-                    {{-- <!-- item-->
-                    <a href="javascript:void(0);" class="dropdown-item notify-item">
-                        <i class="mdi mdi-lock-outline me-1"></i>
-                        <span>Lock Screen</span>
-                    </a> --}}
-
-                    <!-- item-->
-                    <a href="javascript:void(0);" class="dropdown-item notify-item"  onclick="event.preventDefault(); document.getElementById('admin-logout-form').submit();">
-                        <i class="mdi mdi-logout me-1"></i>
-                        <span>Logout</span>
-                    </a>
-                </div>
-            </li>
-
-        </ul>
-        <form id="admin-logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-            @csrf
-        </form>
-        <button class="button-menu-mobile open-left">
-            <i class="mdi mdi-menu"></i>
-        </button>
-        {{-- <div class="app-search dropdown d-none d-lg-block">
-            <form>
-                <div class="input-group">
-                    <input type="text" class="form-control dropdown-toggle"  placeholder="Search..." id="top-search">
-                    <span class="mdi mdi-magnify search-icon"></span>
-                    <button class="input-group-text btn-primary" type="submit">Search</button>
-                </div>
-            </form>
-
-            <div class="dropdown-menu dropdown-menu-animated dropdown-lg" id="search-dropdown">
+<div class="navbar-custom">
+    <ul class="list-unstyled topbar-menu float-end mb-0">
+        <li class="notification-list">
+            <a class="nav-link end-bar-toggle"  title="background process" href="{{ url('/jobs') }}">
+                <i class="dripicons-calendar noti-icon"></i>
+            </a>
+        </li>
+        <li class="dropdown notification-list">
+            <a class="nav-link dropdown-toggle arrow-none" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                <i class="dripicons-bell noti-icon"></i>
+                <span class="noti-icon-badge"></span>
+            </a>
+            <div class="dropdown-menu dropdown-menu-end dropdown-menu-animated dropdown-lg">
                 <!-- item-->
-                <div class="dropdown-header noti-title">
-                    <h5 class="text-overflow mb-2">Found <span class="text-danger">17</span> results</h5>
+                <div class="dropdown-item noti-title">
+                    <h5 class="m-0">
+                        <span class="float-end">
+                            <a href="javascript: void(0);" class="text-dark">
+                                <small>Clear All</small>
+                            </a>
+                        </span>Notification
+                    </h5>
                 </div>
 
-                <!-- item-->
-                <a href="javascript:void(0);" class="dropdown-item notify-item">
-                    <i class="uil-notes font-16 me-1"></i>
-                    <span>Analytics Report</span>
-                </a>
-
-                <!-- item-->
-                <a href="javascript:void(0);" class="dropdown-item notify-item">
-                    <i class="uil-life-ring font-16 me-1"></i>
-                    <span>How can I help you?</span>
-                </a>
-
-                <!-- item-->
-                <a href="javascript:void(0);" class="dropdown-item notify-item">
-                    <i class="uil-cog font-16 me-1"></i>
-                    <span>User profile settings</span>
-                </a>
-
-                <!-- item-->
-                <div class="dropdown-header noti-title">
-                    <h6 class="text-overflow mb-2 text-uppercase">Users</h6>
-                </div>
-
-                <div class="notification-list">
-                    <!-- item-->
+                <div style="max-height: 230px;" data-simplebar>
                     <a href="javascript:void(0);" class="dropdown-item notify-item">
-                        <div class="d-flex">
-                            <img class="d-flex me-2 rounded-circle" src="{{ asset('public/assets/images/users/avatar-2.jpg') }}" alt="Generic placeholder image" height="32">
-                            <div class="w-100">
-                                <h5 class="m-0 font-14">Erwin Brown</h5>
-                                <span class="font-12 mb-0">UI Designer</span>
-                            </div>
-                        </div>
-                    </a>
-
-                    <!-- item-->
-                    <a href="javascript:void(0);" class="dropdown-item notify-item">
-                        <div class="d-flex">
-                            <img class="d-flex me-2 rounded-circle" src="{{ asset('public/assets/images/users/avatar-5.jpg') }}" alt="Generic placeholder image" height="32">
-                            <div class="w-100">
-                                <h5 class="m-0 font-14">Jacob Deo</h5>
-                                <span class="font-12 mb-0">Developer</span>
-                            </div>
-                        </div>
+                        <div class="text-center">There is no new notification.!</div>
                     </a>
                 </div>
+                <a href="javascript:void(0);" class="dropdown-item text-center text-primary notify-item notify-all">
+                    View All
+                </a>
             </div>
-        </div> --}}
-    </div>
-    <!-- end Topbar -->
+        </li>
+
+        @if(userHasAccess('gantt_chart_index') || userHasAccess('cost_estimate_calculation_index'))
+            <li class="dropdown notification-list d-none d-sm-inline-block">
+                <a class="nav-link dropdown-toggle arrow-none" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                    <i class="dripicons-view-apps noti-icon"></i>
+                </a>
+                <div class="dropdown-menu dropdown-menu-end dropdown-menu-animated dropdown-lg p-0">
+
+                    <div class="p-2 shadow-sm border">
+                        
+                        <div class="row g-0 p-0 m-0">
+                            @if(userHasAccess('gantt_chart_index'))
+                                <div class="col p-2">
+                                    <a class="dropdown-icon-item" href="{{ route('admin-gantt-chart-single-view') }}">
+                                        <img src="{{ asset('public/assets/images/gantt-chart.png') }}" alt="slack">
+                                        <span>Gantt chart</span>
+                                    </a>
+                                </div>
+                            @endif
+                            @if(userHasAccess('cost_estimate_calculation_index'))
+                                <div class="col p-2">
+                                    <a class="dropdown-icon-item" href="{{ route('enquiry.calculate-cost-estimation') }}">
+                                        <img src="{{ asset('public/assets/images/calculator.png') }}" alt="slack">
+                                        <span>Calculator</span>
+                                    </a>
+                                </div>
+                            @endif
+                            {{-- <div class="col p-2">
+                                <a class="dropdown-icon-item" href="#">
+                                    <img src="{{ asset('public/assets/images/brands/g-suite.png') }}" alt="G Suite">
+                                    <span>E search</span>
+                                </a>
+                            </div> --}}
+                        </div> <!-- end row-->
+                    </div>
+                    
+                </div>
+            </li>
+        @endif
+
+        <li class="notification-list">
+            <a class="nav-link end-bar-toggle" href="{{ route('setup.roles') }}">
+                <i class="dripicons-gear noti-icon"></i>
+            </a>
+        </li>
+
+        <li class="dropdown notification-list">
+            <a class="nav-link dropdown-toggle nav-user arrow-none me-0 d-flex align-items-center justify-content-between" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false"
+                aria-expanded="false">
+                <span class="account-user-avatar"> 
+                    <img src="{{ asset('public/assets/images/users/avatar-1.jpg') }}" alt="user-image" class="rounded-circle">
+                </span>
+                <span class="account-user-name text-capitalize">{{ Admin()->full_name ?? 'Avatar' }}</span>
+                <i class="bi bi-chevron-down ms-2"></i>
+            </a>
+            <div class="dropdown-menu dropdown-menu-end dropdown-menu-animated topbar-dropdown-menu profile-dropdown">
+                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                    <i class="mdi mdi-account-circle me-1"></i>
+                    <span>My Account</span>
+                </a>
+                <a href="javascript:void(0);" class="dropdown-item notify-item"  onclick="event.preventDefault(); document.getElementById('admin-logout-form').submit();">
+                    <i class="mdi mdi-logout me-1"></i>
+                    <span>Logout</span>
+                </a>
+            </div>
+        </li>
+    </ul>
+    <form id="admin-logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+        @csrf
+    </form>
+    <button class="button-menu-mobile open-left">
+        <i class="mdi mdi-menu"></i>
+    </button>
+</div>
