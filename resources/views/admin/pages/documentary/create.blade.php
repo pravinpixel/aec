694c7cbaa9cb47e370b6c7e8da7f3062fa854157
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-09-20 12:59:51 +0530

    Documentary Merge Menu UI

--- resources/views/admin/pages/documentary/create.blade.php
+++ resources/views/admin/pages/documentary/create.blade.php
@@ -12,71 +12,44 @@
                 @method('POST')
                 @csrf
                 <div class="card shadow-sm border">
-                    <div class="card-header bg-light d-flex align-items-center justify-content-between">
-                        <div class="d-flex align-items-center">
-                            <label class="form-label m-0 px-3" >Title<sup class="text-danger">*</sup></label>
-                            <input type="text" class="form-control col-6"  name="documentary_title" value="{{ old('documentary_title') }}" placeholder="Type Here..." ng-required="true">
+                    <div class="card-header bg-light">
+                        <div class="d-flex align-items-center justify-content-between">
+                            <div class="d-flex align-items-center">
+                                <label class="form-label m-0 px-3" >Title<sup class="text-danger">*</sup></label>
+                                <input type="text" class="form-control col-6"  name="documentary_title" value="{{ old('documentary_title') }}" placeholder="Type Here..." ng-required="true">
+                            </div>
+                            <div>
+                                <a class="btn btn-light btn-sm border shadow-sm"
+                                    data-bs-toggle="collapse" href="#collapseFour"
+                                    aria-expanded="true" aria-controls="collapseFour">
+                                    View merge fields  <i
+                                        class="mdi mdi-chevron-down accordion-arrow"></i>
+                                </a>
+                            </div>
                         </div>
-                        <div>
-                            <a class="btn btn-light btn-sm border shadow-sm"
-                                data-bs-toggle="collapse" href="#collapseFour"
-                                aria-expanded="true" aria-controls="collapseFour">
-                                View merge fields  <i
-                                    class="mdi mdi-chevron-down accordion-arrow"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        <div id="collapseFour" class="collapse show"
+                        <div id="collapseFour" class="collapse"
                             aria-labelledby="headingFour"
                             data-bs-parent="#custom-accordion-one">
-                            <table class="table table-bordered border shadow-sm">
-                                <thead>
-                                    <tr class="bg-light"> 
-                                        <th scope="col"><b>Enquiries</b></th>
-                                        <th scope="col"><b>Customer</b></th>
-                                        <th scope="col"><b>Others</b></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr class="">
-                                        <td scope="row">
-                                            <table>
-                                                @foreach ($enquiries as $name =>  $var)
+                            <div class="row m-0 pt-3">
+                                @foreach (config('documentary.merge_data') as $key => $col)
+                                    <div class="col-6">
+                                        <x-accordion path="false" title="{{ $key }}" open="false">
+                                            <table class="table table-striped border shadow-sm m-0">
+                                                @foreach ($col as $colKey => $var )
                                                     <tr>
-                                                        <td><small>{{ $name }}</small></td> 
-                                                        <td class="px-2">-</td> 
-                                                        <td class="text-primary"><small>{ {{ $var }} }</small></td>
+                                                        <th>{{ $colKey }}</th>
+                                                        <td>-</td>
+                                                        <td style="color: #9500ff">{ {{ $var }} }</td>
                                                     </tr>
-                                                @endforeach
-                                            </table>
-                                        </td>
-                                        <td scope="row">
-                                            <table>
-                                                @foreach ($customers as $name =>  $var)
-                                                    <tr>
-                                                        <td><small>{{ $name }}</small></td> 
-                                                        <td class="px-2">-</td> 
-                                                        <td class="text-primary"><small>{ {{ $var }} }</small></td>
-                                                    </tr>
-                                                @endforeach
-                                            </table>
-                                        </td>
-                                        <td scope="row"  style="vertical-align: top !important">
-                                            <table>
-                                                @foreach ($user_data as $name =>  $var)
-                                                    <tr>
-                                                        <td><small>{{ $name }}</small></td> 
-                                                        <td class="px-2">-</td> 
-                                                        <td class="text-primary"><small>{ {{ $var }} }</small></td>
-                                                    </tr>
-                                                @endforeach
-                                            </table>
-                                        </td>
-                                    </tr> 
-                                </tbody>
-                            </table>
+                                                @endforeach 
+                                            </table> 
+                                        </x-accordion> 
+                                    </div>
+                                @endforeach 
+                            </div>
                         </div>
+                    </div>
+                    <div class="card-body"> 
                         <div>
                             <textarea name="documentary_content" id="textEditor">
                                 <table class="table custom table-borderless">
