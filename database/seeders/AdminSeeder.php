f25749bcfd1405297d668769b9254853f9dc89e2
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-10-26 12:45:59 +0530

    Admin Seeder Profile Changes

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Admin\Employees;
-use Cartalyst\Sentinel\Laravel\Facades\Sentinel;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -16,74 +15,66 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        $admin = Employees::create([
-            'first_name' => 'Arun',
-            'last_name' => 'kalyan',
-            'user_name' => 'arun',
-            'display_name' => 'Arun kalyan',
-            'email' => 'arun.kalyan@aecprefab.net',
-            'password' => Hash::make('12345678'),
-            'mobile_number' => '98786756',
-            // 'share_access' => 1, 
-            'bim_access' => 1, 
-            // 'access' => 1, 
-            'status' => 1,
-            'image' => '',
-            'job_role' => 1,
+        Employees::create([
+            'first_name'       => 'Arun',
+            'last_name'        => 'kalyan',
+            'user_name'        => 'arun',
+            'display_name'     => 'Arun kalyan',
+            'email'            => 'arun.kalyan@aecprefab.net',
+            'password'         => Hash::make('12345678'),
+            'mobile_number'    => '98786756',
+            'bim_access'       => 1,
+            'status'           => 1,
+            'image'            => '',
+            'job_role'         => 1,
             'reference_number' => 'EMP-1',
-            'image'                 => "https://picsum.photos/seed/picsum/200/300"
+            'image'            => "https://picsum.photos/200"
         ]);
-        $admin = Employees::create([
-            'first_name' => 'Super',
-            'last_name' => 'Admin',
-            'user_name' => 'Admin',
-            'display_name' => 'Super Admin',
-            'email' => 'admin@aecprefab.net ',
-            'password' => Hash::make('12345678'),
-            'mobile_number' => '98786757',
-            // 'share_access' => 1, 
-            'bim_access' => 1, 
-            // 'access' => 1, 
-            'status' => 1,
-            'image' => '',
-            'job_role' => 1,
+        Employees::create([
+            'first_name'       => 'Super',
+            'last_name'        => 'Admin',
+            'user_name'        => 'Admin',
+            'display_name'     => 'Super Admin',
+            'email'            => 'admin@aecprefab.net ',
+            'password'         => Hash::make('12345678'),
+            'mobile_number'    => '98786757',
+            'bim_access'       => 1,
+            'status'           => 1,
+            'image'            => '',
+            'job_role'         => 1,
             'reference_number' => 'EMP-2',
-            'image'                 => "https://picsum.photos/seed/picsum/200/300"
+            'image'            => "https://picsum.photos/200"
         ]);
-        $admin = Employees::create([
-            'first_name' => 'aecadmnusr',
-            'last_name' => '2',
-            'user_name' => 'Rosan',
-            'display_name' => 'Rosan',
-            'email' => 'aecadmnusr2@gmail.com',
-            'password' => Hash::make('12345678'),
-            'mobile_number' => '98786758',
-            // 'share_access' => 1, 
-            'bim_access' => 1, 
-            // 'access' => 1, 
-            'status' => 1,
-            'image' => '',
-            'job_role' => 1,
+        Employees::create([
+            'first_name'       => 'aecadmnusr',
+            'last_name'        => '2',
+            'user_name'        => 'Rosan',
+            'display_name'     => 'Rosan',
+            'email'            => 'aecadmnusr2@gmail.com',
+            'password'         => Hash::make('12345678'),
+            'mobile_number'    => '98786758',
+            'bim_access'       => 1,
+            'status'           => 1,
+            'image'            => '',
+            'job_role'         => 1,
             'reference_number' => 'EMP-3',
-            'image'                 => "https://picsum.photos/seed/picsum/200/300"
+            'image'            => "https://picsum.photos/200"
         ]);
 
-        $admin = Employees::create([
-            'first_name' => 'denial',
-            'last_name' => 'dev',
-            'user_name' => 'denial',
-            'display_name' => 'denial',
-            'email' => 'denial@aecprefab.net',
-            'password' => Hash::make('12345678'),
-            'mobile_number' => '98786759',
-            // 'share_access' => 1, 
-            'bim_access' => 1, 
-            // 'access' => 1, 
-            'status' => 1,
-            'image' => '',
-            'job_role' => 5,
+        Employees::create([
+            'first_name'       => 'denial',
+            'last_name'        => 'dev',
+            'user_name'        => 'denial',
+            'display_name'     => 'denial',
+            'email'            => 'denial@aecprefab.net',
+            'password'         => Hash::make('12345678'),
+            'mobile_number'    => '98786759',
+            'bim_access'       => 1,
+            'status'           => 1,
+            'image'            => '',
+            'job_role'         => 5,
             'reference_number' => 'EMP-4',
-            'image'                 => "https://picsum.photos/seed/picsum/200/300"
+            'image'            => "https://picsum.photos/200"
         ]);
     }
 }
