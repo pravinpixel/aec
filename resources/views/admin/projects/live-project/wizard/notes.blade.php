61ea108bcbb186842b3c1a611bab83865bf4099e
k-surendhar <surendhar.pixel@gmail.com>
2022-11-01 18:28:03 +0530

    live proj final wiz.. two teyxt editors

--- resources/views/admin/projects/live-project/wizard/notes.blade.php
+++ resources/views/admin/projects/live-project/wizard/notes.blade.php
@@ -1,8 +1,21 @@
 <div class="p-2 col-md-10 mx-auto my-3">
-    <p class="h5 mt-2">General Notes</p>
-    <div class="form-floating" id="additional_info_text_editor">
+    {{-- <p class="h5 mt-2">General Notes</p> --}}
+    <div class="row">
+        <div class="col">
+            <p class="h5 mt-2">AECPrefab comments</p>
+            <div class="textEditorWrapper d-none">
+                <textarea name="documentary_content" id="aec_admin"></textarea>
+            </div>
+        </div>
+        <div class="col">
+            <p class="h5 mt-2">TrePrefab(customer)comments</p>   
+          <textarea name="documentary_content" id="aec_customer"></textarea>
+        </div>
+    </div>
+    {{-- <div class="form-floating" id="additional_info_text_editor">
         <div dx-html-editor="htmlEditorOptions"> </div>
     </div>
+     --}}
 </div> 
 <div class="row" >
     <div class="col-8">
@@ -33,3 +46,11 @@
     <a ng-click= "storegeneralinfo()" ng-show="SubmitRoute" class="btn btn-primary">Close</a>
     <a ng-click= "submitgeneralinfo()" ng-show="SubmitRoute" class="btn btn-primary" style="display: none;">Complete project</a>
 </div>
+<script> 
+    $(document).ready(function() { 
+        $('.textEditorWrapper').removeClass('d-none');  
+        SetEditor('#aec_admin');
+        SetEditor('#aec_customer');
+    });
+</script>
+
