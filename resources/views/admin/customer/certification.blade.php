a296bd28973166bff7f20ecea9b2e80406ce2067
k-surendhar <surendhar.pixel@gmail.com>
2022-11-25 12:11:50 +0530

    certification page img change

--- resources/views/admin/customer/certification.blade.php
+++ resources/views/admin/customer/certification.blade.php
@@ -5,13 +5,14 @@
         <div class="card-header text-center pt-3 border-0">
             <img src="{{ asset("public/assets/images/logo_customer.png") }}" width="150px" class="mb-2"> <br>
         </div>
-        <div class="card-body pt-0 p-4">       
-            <h3>
-                <img src="{{ asset('public/assets/images/red-tick.png') }}" height="75" width="auto" alt="">
+        <div class="card-body pt-0 p-4">   
+
+            <h3 class="text-center">
+                <img src="{{ asset('public/assets/images/rainbow-tick.png') }}" style="filter: drop-shadow(5px 5px 5px #222);" height="75" width="auto" alt="">
                 Activation Succeeded
             </h3> 
             <hr>
-            <i class="fa fa-check-circle text-primary"  aria-hidden="true"></i><b>Congratulations !</b>
+            <b>Congratulations !</b>
             <p class="pt-2">
               <a href="{{ route('login') }}"> Click here</a> to go back to our login page to proceed further to use AECPrefab services.
             </p>
