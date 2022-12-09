4cf74bd23c35d82432776b7cf265ab0133f7fb8f
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-12-09 11:53:11 +0530

    Chat Box Enter Key Issue

--- public/custom/js/ngControllers/alerts.js
+++ public/custom/js/ngControllers/alerts.js
@@ -1,10 +1,15 @@
 Message = function (type, head) {
     $("#alert").html(`
         <div class="alert alert-custom alert-${type} fade show text-center" role="alert" data-bs-dismiss="alert" aria-label="Close">
-            <strong>${head}</strong> 
+            <strong>${head}</strong>
         </div>
     `);
     $("#alert").fadeTo(3000, 1000).slideUp(1000, function(){
         $("#alert").slideUp(5000);
-    }); 
+    });
+}
+disableEnter = (event) => {
+    if (event.key === "Enter") {
+        event.preventDefault();
+    }
 }
--- resources/views/components/chat-box.blade.php
+++ resources/views/components/chat-box.blade.php
@@ -5,7 +5,7 @@
                 <input type="hidden" name="menu_name" value="'.$menuName.'" placeholder="Example : Project Information ">
                 <input type="hidden" name="module_name" value="'.$moduleName.'"  placeholder="Example : Enquiry">
                 <input type="hidden" name="module_id" value="'.$moduleId.'"  placeholder="Example : Enquiry ID  ">
-                <input type="text" name="message" class="form-control me-1 rounded-pill" placeholder="Type here...">
+                <input type="text" onkeypress="disableEnter(event)" name="message" class="form-control me-1 rounded-pill" placeholder="Type here...">
                 <button onclick="sendMessage(this)" type="button" class="btn rounded-pill btn-sm btn-primary"><i class="fa fa-send"></i></button>
             </div>
         </div>
