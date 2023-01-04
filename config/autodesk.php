aadeba2188dee7857804e70a2086e18fa0a6b3e8
vetri-pixel <vetriselvam.pixel@gmail.com>
2023-01-04 22:20:35 +0530

    added forgeview client credential

--- config/autodesk.php
+++ config/autodesk.php
@@ -2,8 +2,9 @@
             // ->setClientId("AA9tRkeDQP9TmvPPtUG540plagL9XKCH")
             // ->setClientSecret("zyUTW5b1ITHYYSfc");
 return [
-    'public_key'       => 'transient',
-    'client_id'        => 'vUi1doN0vho73XGLoRj6GnyU4IdOs4UO',
-    'client_secret'    => 'zAOFro8yrsIGGUAf',
-    'account_id'       => '51092e70-c456-47a8-8b90-3aa4ee288d08',
+    'public_key'       => env('AUTODESK_PUBLIC_KEY'),
+    'client_id'        => env('AUTODESK_CLIENT_ID'),
+    'client_secret'    => env('AUTODESK_CLIENT_SECRET'),
+    'account_id'       => env('AUTODESK_ACCOUNT_ID'),
+    'bucket_name'      => env('AUTODESK_BUCKET_NAME')
 ];
