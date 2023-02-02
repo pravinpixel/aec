bb08cc9b6ef73092900f57df59dc90813ab351b8
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-02-02 14:11:05 +0530

    FEAT : Issue Messaging  UI setup

--- resources/views/components/chat-box.blade.php
+++ resources/views/components/chat-box.blade.php
@@ -71,8 +71,7 @@
         </div><!-- /.modal-dialog -->
     </div>
 @endif
-
-@if ($status == 'CHAT_BUTTON' && $status != 0)
+@if ($status == 'CHAT_BUTTON')
     <button type="button"  class="btn-sm btn btn-success position-relative" onclick="PreviousChatHistory(this, '{{ $moduleId }}', '{{ $moduleName }}' , '{{ $menuName }}' )" data-bs-toggle="modal" data-bs-target="#viewMyInbox{{ $menuName }}">
         <i class="fa fa-send me-1"></i>
         <span class="cost_estimate_comments_ul">
--- resources/views/live-projects/modals/detail-issue.blade.php
+++ resources/views/live-projects/modals/detail-issue.blade.php
@@ -1,18 +1,7 @@
 <div id="detail-issue-modal" class="modal fade" tabindex="-1" role="dialog"  data-bs-backdrop="static" data-bs-keyboard="false"  aria-labelledby="fullWidthModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-full-width  modal-dialog-scrollable">
-        <div class="modal-content"> 
-            <div class="modal-header">
-                <h4 class="modal-title text-capitalize">
-                    <i class="fa fa-thumb-tack f-20 me-1 text-secondary" style="margin-top: -4px;"></i>
-                    <span id="detail-issue-modal-title"></span>
-                </h4>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-hidden="true"></button>
-            </div>
-            <div class="modal-body" id="detail-issue-modal-content"></div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
+    <div class="modal-dialog modal-lg modal-dialog-scrollable">
+        {{--  modal-dialog-centered  --}}
+        <div class="modal-content" id="detail-issue-modal-content">
         </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
 </div>
