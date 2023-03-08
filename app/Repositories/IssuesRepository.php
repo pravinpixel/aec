4494df0d4cb053a6847e04942667a730310bbfc2
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-03-08 19:26:23 +0530

    Edit issue Function Modal

--- app/Repositories/IssuesRepository.php
+++ app/Repositories/IssuesRepository.php
@@ -2,9 +2,9 @@
 namespace App\Repositories;
 
 use App\Mail\issueCreateMail;
-use App\Mail\liveProject\variationMail;
 use App\Models\Issues;
 use App\Models\Project;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 class IssuesRepository {
     public $Issues,$Project;
@@ -14,7 +14,7 @@ class IssuesRepository {
     }
     public function store($request,$id)
     {
-        $Issues =  $this->Project->findOrFail($id); 
+        $Issues = $this->Project->findOrFail($id); 
         $created_issue = $Issues->Issues()->create([
             "issue_id"      =>  'TIK/' . date('Y') . '/' . (count($Issues->Issues) + 1),
             'title'         => $request->title,
@@ -41,6 +41,33 @@ class IssuesRepository {
         if($request->has('attachments')) {
             foreach ($request->attachments as $key => $attachment) { 
                 $created_issue->IssuesAttachments()->create([
+                    "file_path" => Storage::put('issues',$attachment)
+                ]);
+            }
+        }
+        return true;
+    }
+    public function update($request,$id) {
+        $issue =  $this->Issues->findOrFail($request->issue_id);
+        $issue->update([
+            'title'         => $request->title,
+            'description'   => $request->descriptions,
+            'type'          => $request->assign_type,
+            'request_id'    => $request->requester,
+            'request_name'  => getEmployeeById($request->requester)->display_name,
+            'assignee_id'   => $request->assignee,
+            'assignee_name' => getUser($request->assignee)->first_name,
+            'priority'      => $request->priority,
+            'due_date'      => $request->due_date,
+            'tags'          => json_encode($request->tags),
+        ]); 
+        if($request->has('attachments')) {
+            foreach( $issue->IssuesAttachments as $old_attachment ) {
+                Storage::delete($old_attachment->file_path);
+            }
+            $issue->IssuesAttachments()->delete();
+            foreach ($request->attachments as $key => $attachment) { 
+                $issue->IssuesAttachments()->create([
                     "file_path" => Storage::put('issues',$attachment)
                 ]);
             }
