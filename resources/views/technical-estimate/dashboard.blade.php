0ef6274055b356ead33ccb20767a5d142c16dacf
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-03-14 16:17:15 +0530

    Technical Estimater Dashboard Ui

--- resources/views/technical-estimate/dashboard.blade.php
+++ resources/views/technical-estimate/dashboard.blade.php
@@ -1,46 +1,79 @@
 @extends('layouts.technical-estimate')
 
 @section('admin-content')
-   
-    <div class="content-page" >
+    <div class="content-page">
         <div class="content">
-          
             @include('admin.includes.top-bar')
-
-            <!-- Start Content-->
-            <div class="container-fluid">
-                
-                <!-- start page title -->
-                
-                @include('technical-estimate.includes.page-navigater')
-
-                <!-- end page title -->
-                  <!-- Start Content-->
-                <div class="container-fluid ">
-    
-                    <div class="content container-fluid">
-                        <div class="main"> 
-                            <div class="row">
-                                <div class="col-xl-12  ">
-                                    <div class="row m-0">
-                                        @include('technical-estimate.table')
-                                    </div>
-                                    <!--end card-->
-                                </div> <!-- end col -->
-    
-                            </div> 
+            <div class="container-fluid py-3">
+                <h4 class="mb-3">Dashboard</h4>
+                <div class="row">
+                    <div class="col-md-5">
+                        <div class="row text-center">
+                            <div class="col-12">
+                                <div class="live-status-bg">
+                                    <h3>Total Enquiries</h3>
+                                    <p class="count">{{ $total }}</p>
+                                    <span class="btn btn-primary">
+                                        <i class="mdi mdi-comment-check-outline"></i>
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="live-status-bg">
+                                    <h3>Pending Enquiries</h3>
+                                    <p class="count">{{ $pending }}</p>
+                                    <span class="btn btn-danger">
+                                        <i class="mdi mdi-comment-remove-outline"></i>
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="live-status-bg">
+                                    <h3>Verfied Enquiries</h3>
+                                    <p class="count">{{ $verifed }}</p>
+                                    <span class="btn btn-success">
+                                        <i class="mdi mdi-comment-plus-outline"></i>
+                                    </span>
+                                </div>
+                            </div>
                         </div>
-                        <!-- container --> 
-                    </div> 
-                    
-                </div> 
-            <!-- container -->
-         
+                    </div>
+                    <div class="col-7">
+                        <div class="card border shadow-sm">
+                            <div class="card-body">
+                                <div id="myChart" style="width:100%;height:500px;"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div> 
+    </div>
 @endsection
-
 @push('custom-scripts')
-<script src="{{ asset('public/assets/js/pages/technical-estimate/dashboard.js') }}"></script>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script>
+        google.charts.load('current', {
+            'packages': ['corechart']
+        });
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+            var data = google.visualization.arrayToDataTable([
+                ['Enquiries', 'count'],
+                ['Total Enquiries', {{ $total }}],
+                ['Pending Enquiries', {{ $pending }}],
+                ['Verifed Enquiries', {{ $verifed }}]
+            ]);
+
+            var options = {
+                title: 'Enquiries Dashboard',
+                is3D: true,
+                colors: ['#4199fc', '#cc001a', '#20cf98']
+            };
+
+            var chart = new google.visualization.PieChart(document.getElementById('myChart'));
+            chart.draw(data, options);
+        }
+    </script>
 @endpush
