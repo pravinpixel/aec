2f13213912ca05cb6346a5c3cba40a4bb6de0440
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-03-15 18:25:08 +0530

    test 3

--- app/View/Components/adminLayout.php
+++ app/View/Components/adminLayout.php
@@ -5,14 +5,30 @@ namespace App\View\Components;
 use Illuminate\View\Component;
 
 class adminLayout extends Component
-{ 
-    public function render()
+{
+   public $access = false;
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct()
     {
         if(Admin() !== null) {
-            $access = true;
+            $this->access = true;
         } else {
-            $access = false;
+            $this->access = false;
         }
-        return view('components.admin-layout',compact('access'));
+        
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.admin-layout');
     }
 }
