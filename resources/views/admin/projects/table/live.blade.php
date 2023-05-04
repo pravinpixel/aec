484ea42e910d1fef220b3fd32e421cf7a4bdfc53
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-05-04 15:13:02 +0530

    FIX : Live Project Datatble UI and Action links update

--- resources/views/admin/projects/table/live.blade.php
+++ resources/views/admin/projects/table/live.blade.php
@@ -1,17 +1,19 @@
-<table class="table custom custom dt-responsive nowrap  w-100" id="live-project-table">
-    <thead>
-        <tr>
-            <th class="text-center">@lang('project.project_id')</th> 
-            <th class="text-center">@lang('project.company_name')</th> 
-            <th class="text-center">@lang('project.project_number')</th> 
-            <th class="text-center">@lang('project.contact_person')</th> 
-            <th class="text-center">@lang('project.start_date')</th> 
-            <th class="text-center">@lang('project.delivery_date')</th>
-            <th class="text-center">@lang('project.completed')</th>
-            <th class="text-center">@lang('project.action')</th>
-        </tr>
-    </thead>
-    <tbody> 
-    </tbody>
-</table>
+<div class="table-min-height">
+    <table class="table custom custom dt-responsive nowrap  w-100" id="live-project-table" >
+        <thead>
+            <tr>
+                <th class="text-center">@lang('project.project_id')</th> 
+                <th class="text-center">@lang('project.company_name')</th> 
+                <th class="text-center">@lang('project.project_number')</th> 
+                <th class="text-center">@lang('project.contact_person')</th> 
+                <th class="text-center">@lang('project.start_date')</th> 
+                <th class="text-center">@lang('project.delivery_date')</th>
+                <th class="text-center">@lang('project.completed')</th>
+                <th class="text-center">@lang('project.action')</th>
+            </tr>
+        </thead>
+        <tbody > 
+        </tbody>
+    </table>
+</div>
  
