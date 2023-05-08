d3b364662f65ed66f2c543be8d1e1d28ff9d5c7e
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-05-08 13:04:59 +0530

    Live Project Title update

--- resources/views/admin/includes/head.blade.php
+++ resources/views/admin/includes/head.blade.php
@@ -1,5 +1,5 @@
 <meta charset="utf-8" />
-<title> AEC Prefab | Admin Portal</title>
+<title> AEC Prefab | {{ ucfirst(strtolower(AuthUser())) }} Portal</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
 <meta content="Coderthemes" name="author" />
