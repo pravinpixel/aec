c42c769e40b837a266c6ea8007679b77133bc33f
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-07-03 18:10:59 +0530

    newProposal update

--- app/Http/Controllers/EnquiryProposalController.php
+++ app/Http/Controllers/EnquiryProposalController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\EnquiryProposal;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class EnquiryProposalController extends Controller
 {
@@ -20,10 +21,10 @@ class EnquiryProposalController extends Controller
     }
     public function duplicate($id)
     {
-        $proposal = $this->EnquiryProposal->find($id); 
-        $version = 'R' . (count($proposal->child) + 1);
+        $proposal = $this->EnquiryProposal->find($id);
+        $version  = 'R' . (count($proposal->child) + 1);
         $content  = str_replace(['R0', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6', 'R7', 'R8', 'R9', 'R10', 'R11'], $version, $proposal->content);
-        $newproposal = $this->EnquiryProposal->create([
+        $newProposal  =   $proposal->child()->create([
             "content"         => $content,
             "version"         => $version,
             "created_by"      => AecAuthUser()->full_name,
@@ -31,25 +32,36 @@ class EnquiryProposalController extends Controller
             'title'           => $proposal->title,
             'admin_status'    => "AWAITING",
             'customer_status' => 'NOT_SENT',
-            'parent' => 0
         ]);
+        foreach ($this->EnquiryProposal->find($id)->child as $value) {
+            $value->update([
+                'parent' => $newProposal->id,
+                'admin_status'    => "OBSOLETE",
+            ]);
+        }
         $proposal->update([
-            'admin_status' => 'OBSOLETE',
-            'parent' => $newproposal->id
+            'parent' => $newProposal->id,
+            'admin_status'    => "OBSOLETE",
+        ]);
+        $this->EnquiryProposal->find($newProposal->id)->update([
+            'parent'       => 0,
+            'admin_status' => "AWAITING",
         ]);
         return view('admin.enquiry.wizard.proposal-table', compact('proposal'));
     }
-    public function update($id, Request $request) {
+    public function update($id, Request $request)
+    {
         $result = $this->EnquiryProposal->find($id)->update(['content' => $request->content]);
-        if($result) {
+        if ($result) {
             return response([
                 'status' => true,
                 'message' => 'Changes Saved!'
             ]);
         }
     }
-    public function download($id) {
-        $Proposal = $this->EnquiryProposal->find($id);  
-        return Pdf::loadHTML( $Proposal->content)->setPaper('A4')->download($Proposal->title.'.pdf');
+    public function download($id)
+    {
+        $Proposal = $this->EnquiryProposal->find($id);
+        return Pdf::loadHTML($Proposal->content)->setPaper('A4')->download($Proposal->title . '.pdf');
     }
 }
