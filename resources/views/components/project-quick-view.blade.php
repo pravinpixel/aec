bfba5469a8e5810577a24ac04792663318020567
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-07-04 16:19:17 +0530

    SetDateFormat update

--- resources/views/components/project-quick-view.blade.php
+++ resources/views/components/project-quick-view.blade.php
@@ -196,49 +196,7 @@
                 </div>
             </div>
         </x-accordion>
-        <x-accordion title="Connect Platform" path="false" open="false" argument='null'>
-            {{-- <div class="card-body">
-                <div class="row m-0 justify-content-center align-items-center">
-                    <div class="col-12 row m-0">
-                        <div class="col border p-1 px-2 m-1 shadow rounded-pill border-primary">
-                            <div class="row m-0 align-items-center">
-                                <div class="col-md d-flex align-items-center">
-                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSS3pxBW14LCWAvNVLwZOt8WD6fTVf0q2qi_29x4rX1xqeV-VIZbkVDGndQcK59_VW9tH4&usqp=CAU" width="30px" class="me-3">
-                                    <b>Share Point</b>
-                                </div>
-                                <div class="col-4 text-end">
-                                    <input type="checkbox" id="switch0" disabled data-switch="none"/>
-                                    <label for="switch0" class="border" data-on-label="" data-off-label=""></label>
-                                </div>
-                            </div>
-                        </div> 
-                        <div class="col border p-1 px-2 m-1 shadow rounded-pill border-primary">
-                            <div class="row m-0 align-items-center">
-                                <div class="col-md d-flex align-items-center">
-                                    <img src="https://www.autodesk.com/bim-360/app/themes/bim360/assets/img/favicons/favicon.ico" width="30px" class="me-3">
-                                    <b>BIM 360</b>
-                                </div>
-                                <div class="col-4 text-end">
-                                    <input type="checkbox" id="switch1" disabled data-switch="none"/>
-                                    <label for="switch1" class="border" data-on-label="" data-off-label=""></label>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col border p-1 px-2 m-1 shadow rounded-pill border-primary">
-                            <div class="row m-0 align-items-center">
-                                <div class="col-md d-flex align-items-center">
-                                    <img src="https://i.ytimg.com/an/zRM0HdaPD4zy71zHDYeB2w/featured_channel.jpg?v=5cad0ca7" width="30px" class="me-3">
-                                    <b>24 /7 Office</b>
-                                </div>
-                                <div class="col-4 text-end">
-                                    <input type="checkbox" id="switch2" disabled data-switch="none"/>
-                                    <label for="switch2" class="border" data-on-label="" data-off-label=""></label>
-                                </div>
-                            </div>
-                        </div>
-                    </div> 
-                </div> 
-            </div>   --}}
+        <x-accordion title="Connect Platform" path="false" open="false" argument='null'> 
             <div class="mb-3 shadow-sm">
                 <div class="bg-light py-1 px-2">
                     <b>Share Point Folders</b>
@@ -366,7 +324,7 @@
                                     <label class="col-form-label">Project Start Date</label>
                                 </div>
                                 <div class="col pe-0">
-                                    <div class="form-control form-control-sm  border-0 ">{{ $project['start_date'] }}
+                                    <div class="form-control form-control-sm  border-0 ">{{ SetDateFormat($project['start_date']) }}
                                     </div>
                                 </div>
                             </div>
@@ -378,7 +336,7 @@
                                 </div>
                                 <div class="col pe-0">
                                     <div class="form-control form-control-sm  border-0 ">
-                                        {{ $project['delivery_date'] }}</div>
+                                        {{ SetDateFormat($project['delivery_date']) }}</div>
                                 </div>
                             </div>
                         </div>
