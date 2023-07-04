f8affb1c54d0f3d60920b84ee3ed34a4c45b9e4b
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-07-04 14:18:27 +0530

    code update

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
             @if (trim($slot) === 'AECPrefab')
-                <img src="{{ assets('public/logo-mail.png') }}" style="wi" class="logo" alt="AECPrefab Logo">
+                <img src="{{ asset('public/logo-mail.png') }}" style="wi" class="logo" alt="AECPrefab Logo">
             @else
                 {{ $slot }}
             @endif
