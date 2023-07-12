4ebfe83daeac0af1c3b88ebddac512452a2bf5f5
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-07-12 10:59:19 +0530

    new update

--- resources/views/components/chat-box.blade.php
+++ resources/views/components/chat-box.blade.php
@@ -31,10 +31,7 @@
                     onclick="PreviousChatHistory(this, '{{ $moduleId }}', '{{ $moduleName }}' , '{{ $menuName }}' )"
                     data-bs-toggle="modal" data-bs-target="#viewMyInbox{{ $menuName }}">
                     <i class="mdi mdi-eye me-1"></i> Previous chat history
-                    <small
-                        class="position-absolute text-white top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                        {{ $moduleCount }}
-                    </small>
+                    {!! $moduleCount !!}
                 </button>
             </div>
         @endif
