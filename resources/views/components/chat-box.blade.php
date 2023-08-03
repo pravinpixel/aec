d270b8dc15cd735a9d99788c4e7cc834ac990ea2
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-08-03 18:38:53 +0530

    chat update

--- resources/views/components/chat-box.blade.php
+++ resources/views/components/chat-box.blade.php
@@ -104,15 +104,13 @@
 @if ($status == 'DISABLED')
     <div>
         @include('components.chat-form-input')
-        @if ($PreviousChatHistory != 0)
-            <div class="text-end mt-2">
-                <button type="button" class="btn btn-sm btn-outline-primary position-relative"
-                    onclick="PreviousChatHistory(this, '{{ $moduleId }}', '{{ $moduleName }}' , '{{ $menuName }}' )"
-                    data-bs-toggle="modal" data-bs-target="#viewMyInbox{{ $menuName }}">
-                    <i class="mdi mdi-eye me-1"></i> Previous chat history
-                    {!! $moduleCount !!}
-                </button>
-            </div>
-        @endif
+        <div class="text-end mt-2">
+            <button type="button" class="btn btn-sm btn-outline-primary position-relative"
+                onclick="PreviousChatHistory(this, '{{ $moduleId }}', '{{ $moduleName }}' , '{{ $menuName }}' )"
+                data-bs-toggle="modal" data-bs-target="#viewMyInbox{{ $menuName }}">
+                <i class="mdi mdi-eye me-1"></i> Previous chat history
+                {!! $moduleCount !!}
+            </button>
+        </div>
     </div>
 @endif
