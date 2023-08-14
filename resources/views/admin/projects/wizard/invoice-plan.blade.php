d0220e6cf1c3c980b2368faebc5a4d8f4ccb3ba0
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-08-14 12:12:45 +0530

    FEAT : Invoice UI Update

--- resources/views/admin/projects/wizard/invoice-plan.blade.php
+++ resources/views/admin/projects/wizard/invoice-plan.blade.php
@@ -1,7 +1,37 @@
 <form id="invoicePlan" name="invoicePlanForm" ng-submit="handleSubmitInvoicePlan()">
     <div class="card m-0">
         <div class="card-body">
-            <div class="row mb-3 align-items-center">
+            <table class="table custom table-bordered mb-2">
+                <thead>
+                    <tr>
+                        <th class="fw-normal"><i class="me-2 fa fa-calendar"></i> Start date</th>
+                        <th class="fw-normal"><i class="me-2 fa fa-calendar"></i> End date</th>
+                        <th class="fw-normal"><i class="me-2 fa fa-usd"></i> Project Cost</th>
+                        <th class="fw-normal"><i class="me-2 fa fa-file-text"></i>No.of Invoices</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td ng-bind="project.start_date"></td>
+                        <td ng-bind="project.delivery_date"></td>
+                        <td>
+                            <input calculate-amount type="text" class="form-control"
+                            onkeypress="return isNumber(event)" ng-model="project.project_cost"
+                            placeholder="type here..">
+                        </td>
+                        <td>
+                            <div class="input-group">
+                                <input type="number" class="form-control-sm border rounded" min=1 max=100
+                                    onkeypress="return isNumber(event)" ng-model="project.no_of_invoice"
+                                    placeholder="type here..">
+                                <button type="button" ng-click="handleInvoiceChange()"
+                                    class="btn-sm btn btn-success"><i class="fa fa-refresh"></i> Generate</button>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            {{-- <div class="row mb-3 align-items-center">
                 <div class="col-8">
                     <div class="row align-items-center mb-2 m-0">
                         <div class="col-3"><strong>Project Cost</strong></div>
@@ -35,7 +65,7 @@
                 <div class="col-4 text-center">
                     <div data-provide="datepicker-inline"></div>
                 </div>
-            </div>
+            </div> --}}
             <table class="table custom m-0 custom table-bordered">
                 <thead>
                     <tr>
@@ -51,13 +81,15 @@
                     <tr ng-repeat="invoicePlan in invoicePlans.invoices track by $index">
                         <td class="text-center" ng-bind="invoicePlan.index"></td>
                         <td class="text-center">
-                            <select ng-model="invoicePlan.project_24_id" class="form-select-sm form-select" ng-required="true">
+                            <select ng-model="invoicePlan.project_24_id" class="form-select-sm form-select"
+                                ng-required="true">
                                 <option ng-value="product24.Id" value="@{{ product24.Id }}"
                                     ng-repeat="product24 in project24SevenList" ng-bind="product24.Name"> </option>
                             </select>
                         </td>
                         <td>
-                            <input type="text" ng-model="invoicePlan.invoice_name" class="form-control form-control-sm" placeholder="Type here..."   ng-required="true" />
+                            <input type="text" ng-model="invoicePlan.invoice_name"
+                                class="form-control form-control-sm" placeholder="Type here..." ng-required="true" />
                         </td>
                         <td class="text-center">
                             <div class="position-relative">
@@ -69,17 +101,17 @@
                             </div>
                         </td>
                         <td class="text-end" ng-bind="invoicePlan.amount"></td>
-                        <td class="text-center"><input ng-disabled="invoicePlans.invoices.length == $index + 1" ng-required="true"
-                                type="text" select-on-click onkeypress="return isNumber(event)" name="percentage"
-                                ng-model="invoicePlan.percentage"
+                        <td class="text-center"><input ng-disabled="invoicePlans.invoices.length == $index + 1"
+                                ng-required="true" type="text" select-on-click onkeypress="return isNumber(event)"
+                                name="percentage" ng-model="invoicePlan.percentage"
                                 class="text-center form-control percentage_ form-control-sm border-0 bg-none w-auto mx-auto">
                         </td>
                     </tr>
-                    <tfoot>
-                        <tr>
-                            <th class="text-end" ng-bind="project.project_cost" colspan="5"></th>
-                            <th class="text-center">100</th>
-                        </tr>
+                <tfoot>
+                    <tr>
+                        <th class="text-end" ng-bind="project.project_cost" colspan="5"></th>
+                        <th class="text-center">100</th>
+                    </tr>
                     <tfoot>
             </table>
         </div>
